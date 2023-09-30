35e1a13dd2859b0f6b574684047d87a9b86880e8
Luan <78827184+LuanR44@users.noreply.github.com>
2023-09-30 20:12:30 -0300

    :construction: Correções

--- script.js
+++ script.js
@@ -7,15 +7,14 @@ const incomeDisplay = document.getElementById('income-display');
 const expenseDisplay = document.getElementById('expense-display');
 const totalDisplay = document.getElementById('total-display');
 const transactionTypeButtons = document.querySelectorAll('.transaction-type-button');
-let selectedTransactionType = null
+let selectedTransactionType = null;
 const closeButton = document.querySelector('.close-button');
 const searchInput = document.getElementById('search-input');
 const searchButton = document.getElementById('search-button');
 const dateInput = document.getElementById('date');
-const category = document.getElementById('category').value;
 
 
-// Adiciona um event listener para os botões de tipo de transação
+// Botões do tipo de transação (Entrada ou Saída)
 transactionTypeButtons.forEach(button => {
     button.addEventListener('click', () => {
         selectedTransactionType = button.getAttribute('data-type');
@@ -24,52 +23,51 @@ transactionTypeButtons.forEach(button => {
         button.classList.add('selected');
     });
 });
-// Função para formatar a data de acordo com o padrão brasileiro
+
+
+// Função para formatar a data no padrão brasileiro
 function formatDateToBR(dateString) {
     const [year, month, day] = dateString.split('-');
     return `${day}/${month}/${year}`;
 }
 
-function formatDateToISO(dateString) {
-    const [day, month, year] = dateString.split('/');
-    return `${year}-${month}-${day}`;
-}
 
+// Formata o ano corretamente no input de data
 dateInput.addEventListener('input', (e) => {
     let value = e.target.value;
-
-    
     let parts = value.split('-');
-
-    // Verificar se o ano excede 4 dígitos
     if (parts[0].length > 4) {
         parts[0] = parts[0].substring(0, 4);
         value = parts.join('-');
         e.target.value = value;
     }
 });
+
+
+// Botão para adicionar uma transação 
 addTransactionButton.addEventListener('click', () => {
     document.querySelector("html").dataset.isModalOpen = true;
-
     modalOverlay.style.display = "block";
     modal.style.display = 'block';
 });
 
 closeButton.addEventListener('click', () => {
     document.querySelector("html").dataset.isModalOpen = false;
-
     modalOverlay.style.display = "none";
     modal.style.display = 'none';
 });
 
+
+// Adiciona uma transação através do formulário
 modalForm.addEventListener('submit', (e) => {
     e.preventDefault();
+
     const text = document.getElementById('text').value;
     const amount = parseFloat(document.getElementById('amount').value);
     const date = document.getElementById('date').value;
-    const category = document.getElementById('category').value; // Obtém a categoria selecionada
+    const category = document.getElementById('category').value;
 
-    if (text.trim() === '' || isNaN(amount) || date.trim() === '') {
+    if (text.trim() === '' || isNaN(amount) || date.trim() === '' || !selectedTransactionType) {
         alert('Por favor, preencha todos os campos corretamente.');
         return;
     }
@@ -84,64 +82,81 @@ modalForm.addEventListener('submit', (e) => {
         return;
     }
 
+
     const transaction = {
+        id: Date.now(),
         text,
-        amount: selectedTransactionType === 'expense' ? -amount : amount, // Ajusta o valor com base no tipo de transação
+        amount: selectedTransactionType === 'expense' ? -amount : amount,
         date,
         category,
     };
 
     transactions.push(transaction);
-
     updateTransactionsList();
     updateSummary();
-
     modalForm.reset();
     modalOverlay.style.display = "none";
     modal.style.display = 'none';
+
+    // Redefinir o tipo de transação selecionado
+    selectedTransactionType = null;
+
+    // Remover a classe 'selected' de ambos os botões
+    transactionTypeButtons.forEach(btn => btn.classList.remove('selected'));
 });
 
-// Função para atualizar a lista de transações
+
+// Atualiza a lista de transações na UI
 function updateTransactionsList() {
-    const transactionsListHTML = transactions.map((transaction, index) => {
+    const transactionsListHTML = transactions.map((transaction) => {
         return `
             <li class="transaction ${transaction.amount >= 0 ? 'income' : 'expense'}">
                 <span class="transaction-text">${transaction.text}</span>
                 <span class="transaction-amount">${transaction.amount >= 0 ? `R$ ${transaction.amount.toFixed(2)}` : `- R$ ${Math.abs(transaction.amount).toFixed(2)}`}</span>
                 <span class="transaction-category">${transaction.category}</span>
                 <span class="transaction-data">${formatDateToBR(transaction.date)}</span>
-                <button class="delete-button" data-index="${index}"><i class="ph ph-trash"></i></button>
+                <button class="delete-button" data-id="${transaction.id}"><i class="ph ph-trash"></i></button>
             </li>
         `;
     });
-
     transactionsList.innerHTML = transactionsListHTML.join('');
+    attachDeleteListeners(); // Botões de excluir
+}
 
-    // Adicionar event listener para os botões de exclusão
+
+// Botões de excluir
+function attachDeleteListeners() {
     const deleteButtons = document.querySelectorAll('.delete-button');
     deleteButtons.forEach(button => {
         button.addEventListener('click', (e) => {
-            const index = e.target.getAttribute('data-index');
-            deleteTransaction(index);
+            const id = parseInt(e.target.closest('.delete-button').getAttribute('data-id'));
+            deleteTransactionById(id);
         });
     });
 }
 
-// Função para excluir transação
-function deleteTransaction(index) {
-    transactions.splice(index, 1);
+
+// Deleta uma transação pelo ID
+function deleteTransactionById(id) {
+    const index = transactions.findIndex(t => t.id === id);
+    if (index !== -1) {
+        transactions.splice(index, 1);
+    }
     updateTransactionsList();
     updateSummary();
 }
 
-// Função para atualizar os valores de entradas, saídas e saldo total
+
+// Atualiza o resumo financeiro (Entradas, Saídas e Total)
 function updateSummary() {
     const income = transactions
         .filter(transaction => transaction.amount >= 0)
         .reduce((sum, transaction) => sum + transaction.amount, 0);
+
     const expense = transactions
         .filter(transaction => transaction.amount < 0)
         .reduce((sum, transaction) => sum + transaction.amount, 0);
+
     const total = income + expense;
 
     incomeDisplay.textContent = `R$ ${income.toFixed(2)}`;
@@ -149,87 +164,54 @@ function updateSummary() {
     totalDisplay.textContent = `R$ ${total.toFixed(2)}`;
 }
 
-const transactions = [];
 
-updateTransactionsList();
-updateSummary();
-// Function to filter and display transactions based on the search input
+// Filtra as transações baseado no input de busca
 function filterTransactions() {
-    
-
     const query = searchInput.value.toLowerCase().trim();
-    
-        // If the search input is empty, display all transactions and exit the function
-        if (!query) {
-            displayTransactions(transactions);
-            return;
-        }
-        
-        // Filter the transactions based on the query
-        const filteredTransactions = transactions.filter(transaction => {
-            return transaction.text.toLowerCase().includes(query);
-        });
-    
-        // Update the transactions list with the filtered transactions
-        displayTransactions(filteredTransactions);
+
+    if (!query) {
+        displayTransactions(transactions);
+        return;
     }
-    
-    // Lista de Transação
-    function displayTransactions(transactionsToDisplay) {
-        const transactionsListHTML = transactionsToDisplay.map((transaction, index) => {
-            return `
-                <li class="transaction ${transaction.amount >= 0 ? 'income' : 'expense'}">
-                    <span class="transaction-text">${transaction.text}</span>
-                    <span class="transaction-amount">${transaction.amount >= 0 ? `R$ ${transaction.amount.toFixed(2)}` : `- R$ ${Math.abs(transaction.amount).toFixed(2)}`}</span>
-                    <span class="transaction-category">${transaction.category}</span>
-                    <span class="transaction-data">${formatDateToBR(transaction.date)}</span>
-                    <button class="delete-button" data-index="${index}"><i class="ph ph-trash"></i></button>
-                </li>
-            `;
-        });
-    
-        
-        
-        const fragment = document.createDocumentFragment();
-        
-        transactionsToDisplay.forEach((transaction, index) => {
-            const li = document.createElement('li');
-            li.className = `transaction ${transaction.amount >= 0 ? 'income' : 'expense'}`;
-            li.innerHTML = `
+    const filteredTransactions = transactions.filter(transaction => {
+        return transaction.text.toLowerCase().includes(query);
+    });
+    displayTransactions(filteredTransactions);
+}
+
+
+// Exibe as transações filtradas na UI
+function displayTransactions(transactionsToDisplay) {
+    const transactionsListHTML = transactionsToDisplay.map((transaction) => {
+        return `
+            <li class="transaction ${transaction.amount >= 0 ? 'income' : 'expense'}">
                 <span class="transaction-text">${transaction.text}</span>
                 <span class="transaction-amount">${transaction.amount >= 0 ? `R$ ${transaction.amount.toFixed(2)}` : `- R$ ${Math.abs(transaction.amount).toFixed(2)}`}</span>
                 <span class="transaction-category">${transaction.category}</span>
                 <span class="transaction-data">${formatDateToBR(transaction.date)}</span>
-                <button class="delete-button" data-index="${index}"><i class="ph ph-trash"></i></button>
-            `;
-            fragment.appendChild(li);
-        });
-        
-        transactionsList.innerHTML = '';
-        transactionsList.appendChild(fragment);
-        
-        if (transactionsList.innerHTML !== newHTML) {
-            transactionsList.innerHTML = newHTML;
-        }
-        
-    
-        // Botão Delete
-        const deleteButtons = document.querySelectorAll('.delete-button');
-        deleteButtons.forEach(button => {
-            button.addEventListener('click', (e) => {
-                const index = e.target.getAttribute('data-index');
-                deleteTransaction(index);
-            });
-        });
-    }
-    
-    // Botão de busca
-    searchButton.addEventListener('click', filterTransactions);
-    
-    
-    // Exibe automaticamente todas as transações quando a search bar ficar vazia
-    searchInput.addEventListener('input', function() {
-        if (!searchInput.value.trim()) {
-            displayTransactions(transactions);
-        }
+                <button class="delete-button" data-id="${transaction.id}"><i class="ph ph-trash"></i></button>
+            </li>
+        `;
     });
+    transactionsList.innerHTML = transactionsListHTML.join('');
+    attachDeleteListeners(); // Adiciona listeners aos botões de excluir
+}
+
+// Pesquisa
+searchButton.addEventListener('click', filterTransactions);
+searchInput.addEventListener('input', function() {
+    if (!searchInput.value.trim()) {
+        displayTransactions(transactions);
+    }
+});
+
+searchInput.addEventListener('keyup', function(event) {
+    // Pra funcionar o Enter na busca
+    if (event.key === "Enter") {
+        filterTransactions();
+    }
+});
+
+const transactions = []; // Armazena as transações
+updateTransactionsList();
+updateSummary();
